0d49baa93d5eb7544ea7aadd460fca7ac55d9c2f
Abu-0-RayhaN <rayhan844625@gmail.com>
2024-03-14 02:05:56 +0600

    Array in ts

---
+++ Intro/Array.js
@@ -0,0 +1,12 @@
+var superHeros = [];
+// const heroPower:number[]=[] both are the same to declare
+var heroPower = [];
+var allUsers = [];
+var MlModels = [
+    [22, 33, 33],
+    [3449, 334],
+    [5533, 3344]
+];
+allUsers.push({ name: "", isActive: false });
+superHeros.push('spiderman');
+heroPower.push(2);
---
+++ Intro/Array.ts
@@ -0,0 +1,19 @@
+const superHeros:string[]=[]
+// const heroPower:number[]=[] both are the same to declare
+const heroPower:Array<number>=[]
+
+type singletUser={
+    name:string;
+    isActive:boolean,
+}
+const allUsers:singletUser[]=[]
+
+const MlModels: number[][]=[
+    [22,33,33],
+    [3449,334],
+    [5533,3344]
+]
+
+allUsers.push({name:"",isActive:false})
+superHeros.push('spiderman')
+heroPower.push(2)
