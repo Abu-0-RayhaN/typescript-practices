6ffb06d5a65f40a534a03683a67ca1d143d7dd11
Abu-0-RayhaN <rayhan844625@gmail.com>
2024-03-14 01:14:21 +0600

    obj type

---
+++ Intro/myObj.ts
@@ -0,0 +1,14 @@
+const User = {
+    name:"rayhan",
+    email:"rayhan@lco.dev",
+    isActive:true
+}
+function createUser({name:string, isPaid:boolean}){}
+let newUser={name:"rayhan",isPaid:false,email:"h@h.com"}
+createUser(newUser)
+
+function createCourse():{name:string, price:number}{
+    return {name:"reactjs",price:399}
+}
+
+export{}
