5ebab791551d82aae738501e915b255369826559
Abu-0-RayhaN <rayhan844625@gmail.com>
2024-03-14 22:31:52 +0600

    implements

--- pure-ts/dist/index.js
+++ pure-ts/dist/index.js
@@ -1,9 +1,53 @@
 "use strict";
+// class User{
+//      public email:string
+//     name:string
+//     private readonly city:string=''
+//     constructor(email:string,name:string){
+//         this.email=email
+//         this.name=name
+//     }
+// }
 class User {
     constructor(email, name) {
-        this.city = '';
         this.email = email;
         this.name = name;
+        this.city = '';
     }
 }
 const rayhan = new User("k@g.com", "rayhan");
+//setters and getters
+class aUser {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this._courses = 1;
+        this.city = '';
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class subUser extends aUser {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourses() {
+        this._courses = 4;
+    }
+}
+const rayhaN = new aUser("k@g.com", "rayhan");
--- pure-ts/src/index.ts
+++ pure-ts/src/index.ts
@@ -1,11 +1,52 @@
+// class User{
+//      public email:string
+//     name:string
+//     private readonly city:string=''
+//     constructor(email:string,name:string){
+//         this.email=email
+//         this.name=name
+//     }
+// }
 class User{
-    email:string
-    name:string
-    city:string=''
-    constructor(email:string,name:string){
-        this.email=email
-        this.name=name
+     
+     readonly city:string=''
+    constructor( public email:string, public name:string){
+  
     }
 }
 
 const rayhan=new User("k@g.com","rayhan")
+
+
+//setters and getters
+
+class aUser{
+     private _courseCount=1
+     protected _courses=1
+     readonly city:string=''
+    constructor( public email:string, public name:string){
+    }
+    private deleteToken(){
+        console.log("token deleted");
+    }
+    get getAppleEmail():string{
+        return `apple ${this.email}`
+    }
+    get courseCount():number{
+        return this._courseCount
+    }
+    set courseCount(courseNum:number){
+        if(courseNum<=1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount=courseNum
+    }
+}
+
+class subUser extends aUser{
+    isFamily:boolean=true
+    changeCourses(){
+        this._courses=4
+    }
+}
+const rayhaN=new aUser("k@g.com","rayhan")
